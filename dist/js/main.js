6aa6371380893bde736da3f0e8738b50935cadf0
coffmanjrp <coffmanjrp@gmail.com>
2020-12-15 16:05:12 -0800

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+var counters = document.querySelectorAll('.counter');
+counters.forEach(function (counter) {
+    counter.innerText = '0';
+    var updateCounter = function () {
+        var target = +counter.getAttribute('data-target');
+        var c = +counter.innerText;
+        var increment = target / 200;
+        if (c < target) {
+            counter.innerText = "" + Math.ceil(c + increment);
+            setTimeout(updateCounter, 1);
+        }
+        else {
+            counter.innerText = target.toString();
+        }
+    };
+    updateCounter();
+});
